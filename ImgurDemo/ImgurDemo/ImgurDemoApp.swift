77f621b7e94684803d7fd90d9c77ad0b6e844a82
Marta <martadiazsalas@gmail.com>
2024-05-13 10:34:19 +0200

    Initial commit

---
+++ ImgurDemo/ImgurDemo/ImgurDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImgurDemoApp.swift
+//  ImgurDemo
+//
+//  Created by Marta on 13/5/24.
+//
+
+import SwiftUI
+
+@main
+struct ImgurDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
